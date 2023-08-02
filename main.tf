9833d8065052f29be4adf7a6d937b031e6eee3aa
amitskamboj <amitskamboj@gmail.com>
2023-08-02 19:34:52 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,81 @@
+# Creates an encryption security policy
+resource "aws_opensearchserverless_security_policy" "encryption_policy" {
+  name        = "${var.collection_name}-encryption-policy"
+  type        = "encryption"
+  description = "encryption policy for ${var.collection_name}"
+  policy = jsonencode({
+    Rules = [
+      {
+        Resource = [
+          "collection/${var.collection_name}"
+        ],
+        ResourceType = "collection"
+      }
+    ],
+    AWSOwnedKey = true
+  })
+}
+
+# Creates a collection
+resource "aws_opensearchserverless_collection" "collection" {
+  name = var.collection_name
+  type        = var.collection_type
+  depends_on  = [aws_opensearchserverless_security_policy.encryption_policy]
+  tags        = local.tags
+}
+
+# Creates a network security policy
+resource "aws_opensearchserverless_security_policy" "network_policy" {
+  name        = "${var.collection_name}-network-policy"
+  type        = "network"
+  description = "VPC access for collection endpoint"
+  policy = jsonencode([
+    {
+      Description = "VPC access for collection endpoint",
+      Rules = [
+        {
+          ResourceType = "collection",
+          Resource = [
+            "collection/${var.collection_name}"
+          ]
+        }
+      ],
+      AllowFromPublic = false,
+      SourceVPCEs = [
+        var.vpce_id
+      ]
+    }
+  ])
+}
+
+# Creates a data access policy
+resource "aws_opensearchserverless_access_policy" "data_access_policy" {
+  name        = "${var.collection_name}-data-access-policy"
+  type        = "data"
+  description = "allow index and collection access"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          ResourceType = "index",
+          Resource = [
+            "index/${var.collection_name}/*"
+          ],
+          Permission = [
+            "aoss:*"
+          ]
+        },
+        {
+          ResourceType = "collection",
+          Resource = [
+            "collection/${var.collection_name}"
+          ],
+          Permission = [
+            "aoss:*"
+          ]
+        }
+      ],
+      Principal = var.data_access_policy_principal
+    }
+  ])
+}
---
+++ variables.tf
@@ -0,0 +1,101 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "collection_name" {
+  description = "Name of the OpenSearch Serverless collection."
+}
+
+variable "data_access_policy_principal" {
+  type    = list(string)
+  description = "Opensearch serverless data access policy principal"
+}
+
+variable "vpce_id" {
+  type = string
+}
+
+variable "collection_type" {
+  type = string
+  description = "Opensearch serverless collection type ('SEARCH' or 'TIMESERIES')"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Map of custom tags to pass to the module resources supporting tags"
+  default     = {}
+}
+
+#tags
+variable "tag_name" {
+  description = "Application name tag"
+  type        = string
+  default     = "name testng"
+}
+
+variable "tag_environment" {
+  description = "Application environment tag"
+  type        = string
+  default     = "environment testng"
+}
+
+variable "tag_contact" {
+  description = "Point of contact tag"
+  type        = string
+  default     = "contact testng"
+}
+
+variable "tag_systemtier" {
+  description = "The applicaton's system tier"
+  type        = string
+  default     = "system tier testng"
+}
+
+variable "tag_drtier" {
+  description = "The applicaton's DR tier"
+  type        = string
+  default     = "DR tier testng"
+}
+
+variable "tag_dataclassification" {
+  description = "The application's data classification"
+  type        = string
+  default     = "data classification testng"
+}
+
+variable "tag_budgetcode" {
+  description = "The application's budget code"
+  type        = string
+  default     = "budget code testng"
+}
+
+variable "tag_owner" {
+  description = "The application's owner"
+  type        = string
+  default     = "owner testng"
+}
+
+variable "tag_projectname" {
+  description = "The application's project name"
+  type        = string
+  default     = "project name testng"
+}
+
+variable "tag_notes" {
+  description = "Any notes regarding the application"
+  type        = string
+  default     = "notes testng"
+}
+
+variable "tag_eol" {
+  description = "The application's EOL tag"
+  type        = string
+  default     = "EOL tag testng"
+}
+
+variable "tag_maintwindow" {
+  description = "The application's maintenance window"
+  type        = string
+  default     = "maint window tag testng"
+}
