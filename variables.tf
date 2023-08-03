6d40df59728fd8be5f1eb9400a3f9108dbcaf17e
amitskamboj <amitskamboj@gmail.com>
2023-08-03 18:24:12 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
+  type = string
   default     = "us-east-1"
 }
 
 variable "collection_name" {
+  type = string
   description = "Name of the OpenSearch Serverless collection."
 }
 
